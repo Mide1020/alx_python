2532f43e8101b6f0f5194c15226b5616d29fa889
mide1020 <akinrefonayomide1@gmail.com>
2023-07-18 07:58:48 -0700

    3 last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,1 +1,12 @@
-dhfhfhfv
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+if number > 5:
+    print("last digit of 98 is 8 and is greater than 5")
+
+elif number == 0:
+    print("last digit of -98 is -8 amd is less than 6 and not 0")
+
+else:
+    print("last digit of 0 is zero and is 0")
