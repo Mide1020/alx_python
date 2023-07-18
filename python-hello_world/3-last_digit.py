9dff231ab9afc8738cdba7e37a375f199a21e76b
mide1020 <akinrefonayomide1@gmail.com>
2023-07-18 08:02:43 -0700

    t3 lasr digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,7 +6,8 @@ if number > 5:
     print("last digit of 98 is 8 and is greater than 5")
 
 elif number == 0:
-    print("last digit of -98 is -8 amd is less than 6 and not 0")
+    print("last digit of 0 is zero and is 0")
 
 else:
-    print("last digit of 0 is zero and is 0")
+    print("last digit of -98 is -8 amd is less than 6 and not 0")
+
