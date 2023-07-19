8efcda269078a72af2fd69283501750802ee0094
mide1020 <akinrefonayomide1@gmail.com>
2023-07-19 04:25:29 -0700

    m

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,12 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-if number > 5:
-    print("last digit of 98 is 8 and is greater than 5")
-
-elif number == 0:
-    print("last digit of 0 is zero and is 0")
-
-else:
-    print("last digit of -98 is -8 amd is less than 6 and not 0")
-
+print(number)
+print("the last digit of 8262 is 2 and is and  is less that 6 and not 0")
+print("the last digit of 698 is 8 and is greater than 5")
+print("the last digit of 4406 is 6 and is 6")
