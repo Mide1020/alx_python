138f8d2708f0a83484cc2d96534710ed8104a88d
mide1020 <akinrefonayomide1@gmail.com>
2023-07-19 09:41:03 -0700

    3-last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,15 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
+
 last_digit = abs(number) %10
-string=""
+string = ""
 
 if number < 0:
     last_digit = -last_digit
 
     if last_digit > 5:
         string = "greater than 5"
-    elif last digit == 0:
+    elif last_digit == 0:
         string = "0"
     else:
         string = "less than 6 and not 0"
