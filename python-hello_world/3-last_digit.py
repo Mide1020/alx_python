a6b10570fff29a5440c64c6955282c7878a26025
mide1020 <akinrefonayomide1@gmail.com>
2023-07-20 06:22:40 -0700

    last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,12 +7,10 @@ string = ""
 
 if number < 0:
     last_digit = -last_digit
-
 if last_digit > 5:
     string = "greater than 5"
 elif last_digit == 0:
     string = "0"
 else:
     string = "less than 6 and not 0"
-
 print("Last digit of {}, is {} and is {}".format(number, last_digit, string))
