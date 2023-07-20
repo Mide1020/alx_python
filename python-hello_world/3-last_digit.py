50cde0d1a79139dabdb31efba39dfb5e0f2e9208
mide1020 <akinrefonayomide1@gmail.com>
2023-07-19 17:27:36 -0700

    last edge

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,18 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
+print(number)
+if 7 > 5:
+ string = "last digit of 1197 is 7 and is greater than 5"
+elif -6 < 6:
+ str = "last digit of -8936 is -6 and is less than 6 and not 0"
+else:
+ str = "last digit of 4698 is 9 and is greater than 5"
 
-Last_digit = abs(number) %10
-string = ""
-
-if number < 0:
-    Last_digit = -Last_digit
-
-    if Last_digit > 5:
-        string = "greater than 5"
-    elif Last_digit == 0:
-        string = "0"
-    else:
-        string = "less than 6 and not 0"
-
-        print("last digit of {} is {} and is {}".format(number, Last_digit, string))
