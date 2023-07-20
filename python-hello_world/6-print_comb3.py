2aba17a19fe9c0d9d63d2e425497967960513c96
mide1020 <akinrefonayomide1@gmail.com>
2023-07-20 05:48:35 -0700

    comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for i in range(10):
-    for j in range(i+1, 10):
-        if i < 9:
-            print("{:02d}, ".format(i*10+j), end="")
+    for j in range (i+1, 10):
+        if i == 8 and j == 9:
+            print("{}{}" .format(i, j))
         else:
-            print("{:02d}\n".format(i*10+j), end="")
+            print("{}{}" .format(i, j), end=", ")
