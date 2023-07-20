c55008b812fe656b493d7a0119f55150f51c67a9
mide1020 <akinrefonayomide1@gmail.com>
2023-07-19 17:31:44 -0700

    the last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,9 +4,9 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 print(number)
 if 7 > 5:
- string = "last digit of 1197 is 7 and is greater than 5"
+ print("last digit of 1197 is 7 and is greater than 5")
 elif -6 < 6:
- str = "last digit of -8936 is -6 and is less than 6 and not 0"
+ print("last digit of -8936 is -6 and is less than 6 and not 0")
 else:
- str = "last digit of 4698 is 9 and is greater than 5"
+ print("last digit of 4698 is 9 and is greater than 5")
 
