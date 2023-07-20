7510367a685101bd47e6a16459fe15e51e3fb825
mide1020 <akinrefonayomide1@gmail.com>
2023-07-19 17:00:50 -0700

    1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
-print("Middle word: {}".format("olbero"))
+print("Middle word: {}".format("olberto"))
 
     
 
