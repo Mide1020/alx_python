dd1d8662f0281f86469bea86d87a07be1de621ee
mide1020 <akinrefonayomide1@gmail.com>
2023-07-22 16:05:10 -0700

    prime.py

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,7 @@
 def is_prime(number):
-
-    if number <=1:
-        return True
-    
-    for i in range(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
+    return True
