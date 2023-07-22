cfb86bceb464a3735e36c9cb6b307a9f25760140
mide1020 <akinrefonayomide1@gmail.com>
2023-07-22 15:47:05 -0700

    fibonacci.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,9 +1,9 @@
-def fibonacci_sequence(n)
-if n <= 0:
+def fibonacci_sequence(n):
+ if n <= 0:
     return[]
-elif n == 1:
+ elif n == 1:
     return [0]
-elif n == 2:
+ elif n == 2:
     return[0, 1]
 
 fibonacci_list = [0, 1]
@@ -11,5 +11,5 @@ fibonacci_list = [0, 1]
 while len(fibonacci_list) < n:
     next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
     fibonacci_list.append(next_fibonacci)
+  return fibonacci_list
 
-    return fibonacci_list
