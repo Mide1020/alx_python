c2a8fbdfc3a969f63ad5ac2c748cb3d7d7fb7ecf
mide1020 <akinrefonayomide1@gmail.com>
2023-07-22 11:43:44 -0700

    prime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,8 @@
 def is_prime(number):
+    number = 5
     if number <=1:
         return False
-    number = int(input(enter a number))
+    
     for i in range(2,number):
         if number % i == 0:
             return False
