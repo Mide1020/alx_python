4f37d610a2aef0f8dfef92d5f7f4babdd7c01583
mide1020 <akinrefonayomide1@gmail.com>
2023-07-21 23:45:04 -0700

    string.py

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,4 +1,4 @@
 def reverse_string(string):
-    return reverse_string
-reverse_string = input("please enter a string")
-print(reverse_string[::-1])
+ return reverse_string
+string = "hello"
+print(string[::-1])
