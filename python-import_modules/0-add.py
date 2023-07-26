839f32fa118a933f4c33060e4696bc03639e172e
mide1020 <akinrefonayomide1@gmail.com>
2023-07-26 14:00:35 +0100

    my add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/user/bin/python3
 from add_0 import add
-
 a = 1
 b = 2
 result = add(a, b)
 
 if "__name__ " == "__main__":
 
-  print("{a} + {b} = {result}".format('a', 'b', 'result'))
+        print(f"{a} + {b} = {result}")
