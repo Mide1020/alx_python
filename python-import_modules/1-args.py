2004e2cd0aa499285e32a2b013dcf9037e62ab52
mide1020 <akinrefonayomide1@gmail.com>
2023-07-26 16:27:47 +0100

    arg.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -10,9 +10,8 @@ else:
     if num_args == 1:
      print("1 argument:")
     else:
-        print("{} arguments were passed:".format(num_args))
-
-    for i, arg in enumerate(args, 1):
+        print("{}arguments were passed:".format(num_args))
+for i, arg in enumerate(args, 1):
         print("{}:{}".format(i, arg))
 if __name__  == "__main__":
     print()
