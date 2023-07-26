42f99b0e85dc33fe300e525f94046ebd96db7307
mide1020 <akinrefonayomide1@gmail.com>
2023-07-26 14:03:25 +0100

    my commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 from add_0 import add
 a = 1
 b = 2
-result = add(a, b)
+result = add(1, 2)
 
 if "__name__ " == "__main__":
 
-        print(f"{a} + {b} = {result}")
+        print("1 + 2 = {}".format(result))
