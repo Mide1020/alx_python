f4e02c13c688f37d772860a56fe0cc0ba1d6c18e
mide1020 <akinrefonayomide1@gmail.com>
2023-07-27 23:28:52 +0100

    var.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,3 +9,10 @@ def safe_print_division(a, b):
             print("Inside result: {}".format(result))
         return result
 result = safe_print_division(10, 0)
+#!/usr/bin/python3
+safe_print_division = __import__('3-safe_print_division').safe_print_division
+
+a = 10
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
