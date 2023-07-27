d7865c37eaa3090961a17e20dc89f547cbb6e6b0
mide1020 <akinrefonayomide1@gmail.com>
2023-07-27 23:08:21 +0100

    variable.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,10 +2,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return result
-    except ZeroDivisionError:
         return None
     finally:
-        
         print("Inside result: {}".format(result))
 result = safe_print_division(10, 2)
 
