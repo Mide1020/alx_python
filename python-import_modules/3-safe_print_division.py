4d8c03730fcec7669aeeaf09ae421eaf9542af83
mide1020 <akinrefonayomide1@gmail.com>
2023-07-27 21:43:43 +0100

    print,py

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,10 @@
+def safe_print_division(a, b):
+    try:
+        result = a // b
+        return result
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+
+result = 3-safe_print_division(10, 2)
---
+++ python-import_modules/python-data_structures/0-n0_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    
+    str = " "
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            str += char
+    return str
+element = no_c("school")
+print(element)
