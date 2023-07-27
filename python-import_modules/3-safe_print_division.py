cfc02131fda34f9f9c50af46db6647b4ef3743eb
mide1020 <akinrefonayomide1@gmail.com>
2023-07-27 23:04:18 +0100

    variable.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,9 +1,11 @@
-def divide_integers(a, b):
+def safe_print_division(a, b):
     try:
         result = a / b
         return result
     except ZeroDivisionError:
         return None
     finally:
+        result = safe_print_division(10, 2)
         print("Inside result: {}".format(result))
+
 
