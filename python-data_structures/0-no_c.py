58502c3cce5afdb7ff287cda77fc5344313a9518
mide1020 <akinrefonayomide1@gmail.com>
2023-07-28 13:28:19 +0100

    string.py

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,8 +1,2 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    
-    str = " "
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            str += char
-    return str
+    return my_string.translate({ord('c'):None,ord('C'):None})
