be12aa99dae982b40fa95f0b250afa874eb27b58
mide1020 <akinrefonayomide1@gmail.com>
2023-07-28 11:14:09 +0100

    div.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        if result is not None:
-            print("Inside result: {}".format(result))
-        return result
-result = safe_print_division(10, 0)
-#!/usr/bin/python3
-safe_print_division = __import__('3-safe_print_division').safe_print_division
+def safe_print_division(a, b): 
+    __import__('3-safe_print_division').safe_print_division
 
 a = 10
-b = 0
+b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
