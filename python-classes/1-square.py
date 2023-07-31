964855819e8397e1789c779d584238ecd316083e
mide1020 <akinrefonayomide1@gmail.com>
2023-07-31 13:50:12 +0100

    0-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,18 +5,20 @@ It allows instantiation with an optional size, and provides a method 'area' to c
 The size attribute can be accessed using a property 'size' with a setter for validation.
 
 """
+
 class Square:
-   """A class representing a square.
+      """A class representing a square.
 
     Attributes:
         __size (int): The size of the square.
- Methods:
+
+    Methods:
         _init_(self, size=0): Initializes a square object with an optional size.
         size(self): Getter method to retrieve the size of the square.
         size(self, value): Setter method to set the size of the square.
         area(self): Calculates and returns the area of the square.
     """
-def _init_(self, size=0):
+      def _init_(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
