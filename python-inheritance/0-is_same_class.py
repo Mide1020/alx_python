38b9bc5628f4c5e3ce4676aaaa661bfacaa90b23
mide1020 <akinrefonayomide1@gmail.com>
2023-08-01 12:24:30 +0100

    same.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,21 +1,19 @@
 def is_same_class(obj, a_class):
     """
-    This  function returns True if the object is exactly
-    an instance of the specified class, 
-    otherwise False 
+    This function returns True if the object is exactly an instance of the specified class, otherwise False.
+
     Usage:
     is_same_class(obj, a_class)
 
     Parameters:
-    obj: An object,
-    a_class:A class.
+    obj: An object.
+    a_class: A class.
 
-    Return:
-    True if obj is exactly an instance of a_class, otherwise
-    False
+    Returns:
+    True if obj is exactly an instance of a_class, otherwise False.
     """
-    #Check if the type of obj is equal to a_class
+    # Check if the type of obj is equal to a_class
     if type(obj) == a_class:
         return True
     else:
-     return False
+      return False
