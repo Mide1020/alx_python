1aea6431874387fe14943f8ddd5d23a73520a540
mide1020 <akinrefonayomide1@gmail.com>
2023-08-03 00:59:34 +0100

    same.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,19 +1,30 @@
-def is_same_class(obj, a_class):
+"""This module defines a Square class.
+
+The Square class represents a square with a private instance attribute 'size'.
+It allows instantiation with an optional size, and provides a method 'area' to calculate the square's area.
+The size attribute can be accessed using a property 'size' with a setter for validation.
+
+"""
+
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        _init_(self, size=0): Initializes a square object with an optional size.
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square.
+        area(self): Calculates and returns the area of the square.
     """
-    This function returns True if the object is exactly an instance of the specified class, otherwise False.
+    def _init_(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-    Usage:
-    is_same_class(obj, a_class)
-
-    Parameters:
-    obj: An object.
-    a_class: A class.
-
-    Returns:
-    True if obj is exactly an instance of a_class, otherwise False.
-    """
-    # Check if the type of obj is equal to a_class
-    if type(obj) == a_class:
-        return True
-    else:
-      return False
+    def area(self):
+        return self.__size ** 2
