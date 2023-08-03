6a1307d1db5838a1fda2edc4d2192882352d9451
mide1020 <akinrefonayomide1@gmail.com>
2023-08-03 15:00:54 +0100

    rec.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,33 +1,13 @@
-#!/usr/bin/python3
-"""6-rectangle.py"""
+"""
+writing a class rectangle
+ """
 
-
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """Raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value
-        Args:
-            name (str): name of the parameter
-            value (int): value to validate
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less or equal to 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
+def BaseGeometry(5-base_geometry.py):
+ 
+ class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
 
-    def _init_(self, width, height):
+    def __init__(self, width, height):
         """Instantiates a Rectangle object
         Args:
             width (int): width of the rectangle
@@ -37,5 +17,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-
