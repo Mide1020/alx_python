eb372e275e362fd314b082491f1060296e1f9c7c
mide1020 <akinrefonayomide1@gmail.com>
2023-08-03 15:07:51 +0100

    rec.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,10 +1,30 @@
-"""
-writing a class rectangle
- """
+class BaseGeometry:
+    """BaseGeometry class"""
 
-def BaseGeometry(5-base_geometry.py):
- 
- class Rectangle(BaseGeometry):
+    def area(self):
+        """Calculates the area of the geometry"""
+        raise NotImplementedError("area() is not implemented")
+
+    def perimeter(self):
+        """Calculates the perimeter of the geometry"""
+        raise NotImplementedError("perimeter() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is a positive integer
+        Args:
+            name (str): name of the value being validated
+            value (int): value to be validated
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
@@ -13,7 +33,11 @@ def BaseGeometry(5-base_geometry.py):
             width (int): width of the rectangle
             height (int): height of the rectangle
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        if isinstance(width, int) and isinstance(height, int):
+            if width > 0 and height > 0:
+                self.__width = width
+                self.__height = height
+            else:
+                raise ValueError("width and height must be greater than 0")
+        else:
+            raise TypeError("width and height must be integers")
