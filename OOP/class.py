eaa408b2b4389e5810b180c4a0550fd3280ec0af
mide1020 <akinrefonayomide1@gmail.com>
2023-08-03 11:30:08 +0100

    classs.py

---
+++ OOP/class.py
@@ -0,0 +1,7 @@
+class CarDesign:
+    def __init__(self):
+        print("this car types")
+corrola = CarDesign()
+corrola.name= "toyota"
+print(corrola.name)
+
--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,35 +1,14 @@
-"""This module defines a Square class.
-
-The Square class represents a square with a private instance attribute 'size'.
-It allows instantiation with an optional size, and provides a method 'area' to calculate the square's area.
-The size attribute can be accessed using a property 'size' with a setter for validation.
-
+"""
+same class inheritance task
 """
 
-class Square:
-    """A class representing a square.
 
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        _init_(self, size=0): Initializes a square object with an optional size.
-        size(self): Getter method to retrieve the size of the square.
-        size(self, value): Setter method to set the size of the square.
-        area(self): Calculates and returns the area of the square.
+def is_same_class(obj, a_class):
     """
-    def is_same_class(obj, a_class):
-     """
-    This function checks if an object is exactly an instance of a specified class.
+    Checks if the class the object is the same as the class of the class.
 
-    Args:
-    obj: Any object.
-    a_class: Any class.
-
-    Returns:
-    True if the object is exactly an instance of the specified class, otherwise False.
     """
-     if type(obj) == a_class:
+    if obj._class.__name_ == a_class._name_:
         return True
-     else:
-        return False
+    else:
+        return type(obj) == a_class
