b7f5d3f529201147a480a59c0d6cd6c711e2dbd6
mide1020 <akinrefonayomide1@gmail.com>
2023-08-05 20:41:20 +0100

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,14 @@
 class Base:
+    """Base class for all future classes in this project.
+    Manages the id attribute in all future classes and avoids duplicating the same code and bugs
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initializes a new instances of the Base class.
+        Args:
+        id(int):The ID of instances.
+        """
         if id is not None:
             self.id = id
         else:
