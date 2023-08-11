0dfde12309c8b096dcd203f2b1fe96ae7f0b322e
mide1020 <akinrefonayomide1@gmail.com>
2023-08-11 14:15:46 +0100

    5-json.py

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+
+if len(sys.argv) == 1:
+    q = ""
+else:
+    q = sys.argv[1]
+
+url = "http://0.0.0.0:5000/search_user"
+data = {"q": q}
+
+response = http://requests.post(url, data=data)
+
+try:
+    json_response = response.json()
+    if json_response:
+        print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
