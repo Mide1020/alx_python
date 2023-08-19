bb2b4ae9a9307b47b83e5819f2b23738fb414285
mide1020 <akinrefonayomide1@gmail.com>
2023-08-19 13:29:36 +0100

    task 7

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,36 +1,137 @@
-"""this is a class rectangle that inherits baseGeometric
+#!/usr/bin/python3
+
 """
 
+Module 7-rectangle: This module defines the Rectangle class.
+
+"""
 
 class BaseGeometry:
-    """A class representing a base geometry.
 
+    """
+
+    BaseGeometry class provides a basic structure for geometrical calculations.
+
+    Public Methods:
+
+    - area(): Raises an exception indicating that area calculation is not implemented.
 
     """
 
     def area(self):
-        """Calculates the area of the geometry.
+
+        """
+
+        Calculate the area of the geometry (not implemented).
 
         Raises:
-            Exception: If area is not implemented.
 
-        Returns:
-            The area of the geometry.
+            Exception: Indicates that the area() method is not implemented.
+
         """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates if a value is an integer and is greater than 0.
+
+        """
+
+        Validate if a given value is a positive integer.
 
         Args:
-            name (str): The name of the value.
+
+            name (str): The name of the value being validated.
+
             value (int): The value to be validated.
 
         Raises:
+
             TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
+
+            ValueError: If value is not a positive integer.
+
         """
+
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+
+            raise TypeError(f"{name} must be an integer")
+
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+
+            raise ValueError(f"{name} must be a positive integer")
+
+class Rectangle(BaseGeometry):
+
+    """
+
+    Rectangle class inherits from BaseGeometry and represents a rectangle.
+
+    Attributes:
+
+        __width (int): The width of the rectangle.
+
+        __height (int): The height of the rectangle.
+
+    Public Methods:
+
+    - __init__(self, width, height): Initializes a Rectangle instance.
+
+    """
+
+    def __init__(self, width, height):
+
+        """
+
+        Initialize a Rectangle instance.
+
+        Args:
+
+            width (int): The width of the rectangle.
+
+            height (int): The height of the rectangle.
+
+        """
+
+        self.integer_validator("width", width)
+
+        self.integer_validator("height", height)
+
+        self.__width = width
+
+        self.__height = height
+
+    def __str__(self):
+
+        """
+
+        Return a string representation of the rectangle.
+
+        Returns:
+
+            str: The rectangle description in the format [Rectangle] <width>/<height>.
+
+        """
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+
+        """
+
+        Calculate and return the area of the rectangle.
+
+        Returns:
+
+            int: The area of the rectangle.
+
+        """
+
+        return self.__width * self.__height
+
+if __name__ == "__main__":
+
+    r = Rectangle(3, 5)
+
+    print(r)
+
+    print(r.area())
--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -3,7 +3,7 @@ import sys
 
 if len(sys.argv) != 3:
     print("Usage: {} <mide1020> <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>".format(sys.argv[0]))
-    sys.exit(1)
+)    sys.exit(1)
 
 username = sys.argv[1]
 access_token = sys.argv[2]
