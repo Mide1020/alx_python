1d448b90bf4581a184c5d609639e444709a2da50
mide1020 <akinrefonayomide1@gmail.com>
2023-08-19 13:47:15 +0100

    task 6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,48 +1,139 @@
-"""this is a class rectangle that inherits from basegeometry
- """
+#!/usr/bin/python3
 
+"""
+
+Module 6-rectangle: This module defines the Rectangle class.
+
+"""
 
 class BaseGeometry:
-    """BaseGeometry class"""
+
+    """
+
+    BaseGeometry class provides a basic structure for geometrical calculations.
+
+    Public Methods:
+
+    - area(): Raises an exception indicating that area calculation is not implemented.
+
+    """
 
     def area(self):
-        """Calculates the area of the geometry"""
-        raise NotImplementedError("area() is not implemented")
 
-    def perimeter(self):
-        """Calculates the perimeter of the geometry"""
-        raise NotImplementedError("perimeter() is not implemented")
+        """
+
+        Calculate the area of the geometry (not implemented).
+
+        Raises:
+
+            Exception: Indicates that the area() method is not implemented.
+
+        """
+
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is a positive integer
-        Args:
-            name (str): name of the value being validated
-            value (int): value to be validated
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than or equal to 0
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
+        """
+
+        Validate if a given value is a positive integer.
+
+        Args:
+
+            name (str): The name of the value being validated.
+
+            value (int): The value to be validated.
+
+        Raises:
+
+            TypeError: If value is not an integer.
+
+            ValueError: If value is not a positive integer.
+
+        """
+
+        if not isinstance(value, int):
+
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+
+            raise ValueError(f"{name} must be a positive integer")
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+
+    """
+
+    Rectangle class inherits from BaseGeometry and represents a rectangle.
+
+    Attributes:
+
+        __width (int): The width of the rectangle.
+
+        __height (int): The height of the rectangle.
+
+    Public Methods:
+
+    - __init__(self, width, height): Initializes a Rectangle instance.
+
+    """
 
     def __init__(self, width, height):
-        """Instantiates a Rectangle object
-        Args:
-            width (int): width of the rectangle
-            height (int): height of the rectangle
-        """
-        if isinstance(width, int) and isinstance(height, int):
-            if width > 0 and height > 0:
-                self.__width = width
-                self.__height = height
-            else:
-                raise ValueError("width and height must be greater than 0")
-        else:
-            raise TypeError("width and height must be integers")
 
+        """
+
+        Initialize a Rectangle instance.
+
+        Args:
+
+            width (int): The width of the rectangle.
+
+            height (int): The height of the rectangle.
+
+        """
+
+        self.integer_validator("width", width)
+
+        self.integer_validator("height", height)
+
+        self.__width = width
+
+        self.__height = height
+
+    def __str__(self):
+
+        """
+
+        Return a string representation of the rectangle.
+
+        Returns:
+
+            str: The rectangle description in the format [Rectangle] <width>/<height>.
+
+        """
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+if __name__ == "__main__":
+
+    r = Rectangle(3, 5)
+
+    print(r)
+
+    print(dir(r))
+
+    try:
+
+        print("Rectangle: {} - {}".format(r.width, r.height))
+
+    except Exception as e:
+
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+
+        r2 = Rectangle(4, True)
+
+    except Exception as e:
+
+        print("[{}] {}".format(e.__class__.__name__, e))
