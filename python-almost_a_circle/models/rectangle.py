2683ccb1e47322e9f21ea288ab0f007203259cbd
mide1020 <akinrefonayomide1@gmail.com>
2023-08-21 09:16:45 +0100

    task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -106,9 +106,28 @@ class Rectangle(Base):
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
+    def update(self, *args):
+        """Update the attributes of the rectangle with the provided arguments.
+
+        Args:
+            *args: Variable number of arguments in the order: id, width, height, x, y.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
 # Example usage
 if __name__ == "__main__":
     r = Rectangle(10, 5, 2, 3, 1)
+    print("Before update:")
     print(r)
-    print("Area:", r.area())
-    r.display()
+    r.update(2, 8, 6)
+    print("After update:")
+    print(r)
