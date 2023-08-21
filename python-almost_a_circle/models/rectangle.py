0914d334623e7099faa2d8b7029bd6bb3b6986cd
mide1020 <akinrefonayomide1@gmail.com>
2023-08-21 08:51:09 +0100

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -23,6 +23,7 @@ class Rectangle(Base):
 
         Raises:
             ValueError: If width or height is not positive, or if x or y is negative.
+            TypeError: If width, height, x, or y is not an integer.
         """
         super().__init__(id)
         self.width = width
@@ -37,8 +38,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer.")
         if value <= 0:
-            raise ValueError("Width must be positive.")
+            raise ValueError("Width must be > 0.")
         self.__width = value
 
     @property
@@ -48,8 +51,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer.")
         if value <= 0:
-            raise ValueError("Height must be positive.")
+            raise ValueError("Height must be > 0.")
         self.__height = value
 
     @property
@@ -59,8 +64,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("X coordinate must be an integer.")
         if value < 0:
-            raise ValueError("X coordinate cannot be negative.")
+            raise ValueError("X coordinate must be >= 0.")
         self.__x = value
 
     @property
@@ -70,8 +77,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Y coordinate must be an integer.")
         if value < 0:
-            raise ValueError("Y coordinate cannot be negative.")
+            raise ValueError("Y coordinate must be >= 0.")
         self.__y = value
 
     def area(self):
@@ -96,9 +105,3 @@ class Rectangle(Base):
             str: The string representation of the rectangle.
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
-
-# Example usage
-if __name__ == "__main__":
-    r = Rectangle(10, 5, 2, 7, 1)
-    print(r)
-    r.display()
