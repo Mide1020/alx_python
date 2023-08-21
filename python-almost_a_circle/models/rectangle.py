e670b1db51ea71548a6dce1a19e686636afc4505
mide1020 <akinrefonayomide1@gmail.com>
2023-08-21 09:23:06 +0100

    task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,117 +11,32 @@ class Rectangle(Base):
     """A class representing a Rectangle that inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initialize a Rectangle instance.
+        # (Constructor code as before...)
+
+    # Other methods as before...
+
+    def update(self, *args, **kwargs):
+        """Update the attributes of the rectangle using arguments and keyword arguments.
 
         Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the top-left corner. Defaults to 0.
-            y (int, optional): The y-coordinate of the top-left corner. Defaults to 0.
-            id (int, optional): The ID of the rectangle. If not provided, a new ID will be generated.
-
-        Raises:
-            ValueError: If width or height is not positive, or if x or y is negative.
-            TypeError: If width, height, x, or y is not an integer.
+            *args: Positional arguments in the order: id, width, height, x, y.
+            **kwargs: Keyword arguments as key/value pairs representing attributes.
         """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        """int: The width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Width must be an integer.")
-        if value <= 0:
-            raise ValueError("Width must be > 0.")
-        self.__width = value
-
-    @property
-    def height(self):
-        """int: The height of the rectangle."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Height must be an integer.")
-        if value <= 0:
-            raise ValueError("Height must be > 0.")
-        self.__height = value
-
-    @property
-    def x(self):
-        """int: The x-coordinate of the top-left corner."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("X coordinate must be an integer.")
-        if value < 0:
-            raise ValueError("X coordinate must be >= 0.")
-        self.__x = value
-
-    @property
-    def y(self):
-        """int: The y-coordinate of the top-left corner."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Y coordinate must be an integer.")
-        if value < 0:
-            raise ValueError("Y coordinate must be >= 0.")
-        self.__y = value
-
-    def area(self):
-        """Calculate the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self.width * self.height
-
-    def display(self):
-        """Display the rectangle using '#' characters, considering x and y."""
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def __str__(self):
-        """Return a string representation of the rectangle.
-
-        Returns:
-            str: The string representation of the rectangle.
-        """
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
-
-    def update(self, *args):
-        """Update the attributes of the rectangle with the provided arguments.
-
-        Args:
-            *args: Variable number of arguments in the order: id, width, height, x, y.
-        """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
 
 # Example usage
 if __name__ == "__main__":
@@ -129,5 +44,8 @@ if __name__ == "__main__":
     print("Before update:")
     print(r)
     r.update(2, 8, 6)
-    print("After update:")
+    print("After positional update:")
+    print(r)
+    r.update(x=4, y=2, width=12)
+    print("After keyword update:")
     print(r)
