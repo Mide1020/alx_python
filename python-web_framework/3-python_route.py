dff0fe0c1ae104953a4f02d43cbcc23e8526ad3e
mide1020 <akinrefonayomide1@gmail.com>
2023-08-28 19:25:46 +0100

    task 3

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,14 +1,17 @@
 """
 Flask Web Application
 
-This script sets up a Flask web application with multiple routes for different
-messages and text display.
+This script sets up a Flask web application with multiple routes for displaying
+different messages based on URL patterns.
 
 Routes:
     /: Display "Hello HBNB!"
     /hbnb: Display "HBNB"
-    /c/<text>: Display "C " followed by the value of the text variable (replace
+    /c/<text>: Display "C ", followed by the value of the text variable (replace
                 underscore _ symbols with a space)
+    /python/<text>: Display "Python ", followed by the value of the text variable
+                    (replace underscore _ symbols with a space ). If text is not
+                    provided, the default value "is cool" is used.
 
 Usage:
     Run this script to start the Flask web application. Access the application
@@ -16,6 +19,8 @@ Usage:
     - http://localhost:5000/ for "Hello HBNB!"
     - http://localhost:5000/hbnb for "HBNB"
     - http://localhost:5000/c/your_text_here for "C your text here"
+    - http://localhost:5000/python/your_text_here for "Python your text here"
+    - http://localhost:5000/python/ for "Python is cool"
 """
 
 from flask import Flask
@@ -55,6 +60,22 @@ def c_text(text):
              underscores (_) replaced by spaces.
     """
     return "C " + text.replace("_", " ")
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    Route handler for URLs with the '/python/<text>' pattern.
+
+    Args:
+        text (str, optional): The text provided in the URL. If not provided, uses
+                              the default value "is cool".
+
+    Returns:
+        str: The message "Python " followed by the value of the text variable, with
+             underscores (_) replaced by spaces.
+    """
+    return "Python " + text.replace("_", " ")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
