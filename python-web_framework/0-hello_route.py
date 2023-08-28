6c6b0bae9676b85ee4b7955e1bc56ca2a1ab6aa2
mide1020 <akinrefonayomide1@gmail.com>
2023-08-28 18:08:14 +0100

    task 0

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,9 +1,30 @@
+"""
+Flask Web Application
+
+This script sets up a simple Flask web application with a single route
+that displays "Hello HBNB!" when accessed.
+
+Routes:
+    /: Display "Hello HBNB!"
+
+Usage:
+    Run this script to start the Flask web application. Access the application
+    by opening a web browser and navigating to http://localhost:5000/.
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route handler for the root URL ('/')
+
+    Returns:
+        str: The message "Hello HBNB!" to be displayed in the browser.
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
