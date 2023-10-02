23c0b36a538c71e955a5da478f439b9cbc631eab
mide1020 <akinrefonayomide1@gmail.com>
2023-10-02 18:05:48 +0100

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,45 +1,46 @@
 import requests
+import sys
 
 def get_employee_info(employee_id):
     # Define the base URL for the API
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Make a request to get employee details
+    # Make a GET request to the employee details endpoint
     employee_url = f"{base_url}/users/{employee_id}"
     response = requests.get(employee_url)
 
-    # Check if the request was successful
     if response.status_code != 200:
         print(f"Failed to retrieve employee information for ID {employee_id}")
         return
 
     employee_data = response.json()
-    employee_name = employee_data["name"]
+    employee_name = employee_data.get("name")
 
-    # Make a request to get the employee's TODO list
-    todos_url = f"{base_url}/users/{employee_id}/todos"
-    response = requests.get(todos_url)
+    # Make a GET request to the employee's TODO list endpoint
+    todo_url = f"{base_url}/users/{employee_id}/todos"
+    response = requests.get(todo_url)
 
-    # Check if the request was successful
     if response.status_code != 200:
         print(f"Failed to retrieve TODO list for ID {employee_id}")
         return
 
-    todos = response.json()
+    todo_list = response.json()
 
     # Calculate the number of completed tasks
-    completed_tasks = [todo for todo in todos if todo["completed"]]
+    completed_tasks = [task for task in todo_list if task["completed"]]
     num_completed_tasks = len(completed_tasks)
-    total_num_tasks = len(todos)
+    total_num_tasks = len(todo_list)
 
-    # Print the employee's TODO list progress
+    # Print the employee information
     print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
-    
+
     # Print the titles of completed tasks
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    employee_id = int(input("Enter the employee ID: "))
-    get_employee_info(employee_id)
-    
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_info(employee_id)
