f0428fb241bc9bfb76febe9a158b2f9f5f83a668
mide1020 <akinrefonayomide1@gmail.com>
2023-10-03 00:00:11 +0100

    task 0

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,54 +1,49 @@
 import requests
-import sys
-import csv
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Construct URLs for employee details and TODO list
+    # Construct URLs for employee details and TODO items
     employee_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/users/{employee_id}/todos"
 
     try:
         # Fetch employee details
         employee_response = requests.get(employee_url)
+        employee_response.raise_for_status()
         employee_data = employee_response.json()
 
-        # Fetch TODO list
+        # Fetch TODO list for the employee
         todo_response = requests.get(todo_url)
-        todo_list = todo_response.json()
+        todo_response.raise_for_status()
+        todo_data = todo_response.json()
 
         # Calculate progress
-        total_tasks = len(todo_list)
-        completed_tasks = sum(1 for task in todo_list if task["completed"])
+        total_tasks = len(todo_data)
+        completed_tasks = sum(1 for task in todo_data if task['completed'])
 
-        # Display the progress in the correct format
+        # Display progress
         print(f"Employee {employee_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
-
-        # Display the titles of completed tasks
-        completed_task_titles = [task["title"] for task in todo_list if task["completed"]]
-        for title in completed_task_titles:
-            print(f"\t{title}")
-
-        # Export data to CSV
-        csv_filename = f"employee_{employee_id}_todo.csv"
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(["Employee Name", "Total Tasks", "Completed Tasks"])
-            csv_writer.writerow([employee_data['name'], total_tasks, completed_tasks])
-            csv_writer.writerow(["Completed Task Titles"])
-            csv_writer.writerows([[title] for title in completed_task_titles])
         
-        print(f"Data exported to {csv_filename}")
+        # Display completed tasks
+        for task in todo_data:
+            if task['completed']:
+                print(f"\t{task['title']}")
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
-    except ValueError as e:
-        print(f"Error parsing JSON response: {e}")
 
 if __name__ == "__main__":
+    import sys
+
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
-    else:
+        sys.exit(1)
+
+    try:
         employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    get_employee_todo_progress(employee_id)
