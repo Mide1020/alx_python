ec93af05ed67a12e9fc9aa37689b3f2f3cd7774b
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 00:11:28 +0100

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -6,13 +6,11 @@ num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name itself
 
 # Print the number of arguments and the list of arguments
 if num_args == 0:
-    print("Number of argument(s): 0.")
-    print(".")
+    print("No arguments.")
+elif num_args == 1:
+    print("1 argument:")
 else:
-    print(f"Number of argument(s): {num_args}.")
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
+    print(f"{num_args} arguments:")
 
-if __name__ == "__main__":
-    # Additional code to execute if this script is run as the main program
-    pass
+for i, arg in enumerate(sys.argv[1:], start=1):
+    print(f"{i}: {arg}")
