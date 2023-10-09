01b3a3267a819d26848ec7419aadbba4c49b93e8
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 00:59:43 +0100

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 import sys
+def print_args():
+    # Get the number of arguments
+    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name itself
 
-# Get the number of arguments
-num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name itself
+    # Print the number of arguments and the list of arguments
+    if num_args == 1:
+        print(f"1 argument:")
+    else:
+        print(f"{num_args} arguments:")
 
-# Print the number of arguments and the list of arguments
-if num_args == 1:
-    print(f"1 argument:")
-else:
-    print(f"{num_args} arguments:")
+    # Print the arguments, if any
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
 
-# Print the arguments, if any
-for i, arg in enumerate(sys.argv[1:], start=1):
-    print(f"{i}: {arg}")
+if __name__ == "__main__":
+    print_args()
