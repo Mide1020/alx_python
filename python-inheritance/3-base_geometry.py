b93cd52fd553fba6cb8ec6abee884ee1db6381d9
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 01:44:23 +0100

    takj 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,7 @@
-"""writing an empty class
-"""
-
 class BaseGeometry:
- """this class doesn't have any attribute or method defined in it 
- """
- pass
-
+    """
+    This is an empty base class for geometry-related functionality.
+    It can be used as a base class to define specific geometric shapes
+    or operations in derived classes.
+    """
+    pass
