58b2b9a10062ac978c8c07567c5c54e6b4bdd239
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 01:02:56 +0100

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 import sys
+
 def print_args():
     # Get the number of arguments
     num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name itself
 
     # Print the number of arguments and the list of arguments
     if num_args == 1:
-        print(f"1 argument:")
-    else:
+        print("1 argument:")
+    elif num_args > 1:
         print(f"{num_args} arguments:")
+    else:
+        print("0 arguments.")
 
     # Print the arguments, if any
     for i, arg in enumerate(sys.argv[1:], start=1):
