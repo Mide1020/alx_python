52f93181353d8f02e4ac2f7bd41285bef413f5b5
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 01:57:35 +0100

    task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -15,8 +15,7 @@ class BaseGeometry:
         or operations in derived classes. It provides a common base for organizing
         geometry-related functionality without any specific implementation.
     """
-
-    def __repr__(self):
+def __repr__(self):
         """
         Returns a string representation of the BaseGeometry instance.
 
@@ -24,11 +23,9 @@ class BaseGeometry:
             str: A string representation of the instance.
         """
         return "<{} object at {}>".format(type(self).__module__ + "." + type(self).__name__, hex(id(self)))
-
 # Example usage:
 if __name__ == "__main__":
     bg = BaseGeometry()
-
     print(bg)
     print(dir(bg))
     print(dir(BaseGeometry))
