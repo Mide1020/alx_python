7b9c47a3eb9c5a45879d3db3006ca1195451b4d7
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 01:12:40 +0100

    3-safe

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,14 +2,12 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        operation = f"{a} / {b} = {result}"
     except ZeroDivisionError:
         result = None
-        operation = f"{a} / {b} is undefined (division by zero)"
     finally:
         print("Inside result: {}".format(result))
         if result is not None:
-            print("{}".format(operation))
+            print("{} / {} = {}".format(a, b, result))
 
 # Example usage:
 a = 10
