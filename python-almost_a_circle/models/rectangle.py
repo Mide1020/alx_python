466d73fb6dcbc04128a30cca32e14b34ad334800
mide1020 <akinrefonayomide1@gmail.com>
2023-10-10 02:06:20 +0100

    trec.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,44 +1,82 @@
 from models.base import Base
-
-class Rectangle(Base):
-    """A class representing a Rectangle that inherits from Base."""
+class Rectangle(BaseGeometry):
+    """
+    Represents a rectangle.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        # (Constructor code as before...)
-
-    # Other methods as before...
-
-    def update(self, *args, **kwargs):
-        """Update the attributes of the rectangle using arguments and keyword arguments.
+        """
+        Initializes a Rectangle instance.
 
         Args:
-            *args: Positional arguments in the order: id, width, height, x, y.
-            **kwargs: Keyword arguments as key/value pairs representing attributes.
-        """
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x-coordinate of the rectangle's position.
+            y (int): The y-coordinate of the rectangle's position.
+            id (int): The ID of the rectangle.
 
-# Example usage
-if __name__ == "__main__":
-    r = Rectangle(10, 5, 2, 3, 1)
-    print("Before update:")
-    print(r)
-    r.update(2, 8, 6)
-    print("After positional update:")
-    print(r)
-    r.update(x=4, y=2, width=12)
-    print("After keyword update:")
-    print(r)
+        Raises:
+            TypeError: If width, height, x, or y is not an integer.
+            ValueError: If width or height is not greater than 0, or if x or y is less than 0.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x."""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y."""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
