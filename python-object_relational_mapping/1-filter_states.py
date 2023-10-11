4e679259fbfde64ff3b6763222ea884c1864a1a0
mide1020 <akinrefonayomide1@gmail.com>
2023-10-11 03:20:51 +0100

    1-filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,45 @@
+#!/bin/usr/python3
+"""
+This module list all state that starts with N
+"""
+
 import MySQLdb
 import sys
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 4:
-    print("Usage: python list_states_with_N.py <mysql_username> <mysql_password> <database_name>")
-    sys.exit(1)
 
-# Get command-line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+def list_states_with_N(username, password, database):
+    """Connect to the MySQL server"""
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-# Connect to the MySQL server
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=mysql_username,
-    passwd=mysql_password,
-    db=database_name
-)
+    """Create a cursor object to interact with the database"""
+    cursor = db.cursor()
 
-# Create a cursor object to interact with the database
-cursor = db.cursor()
+    """Execute the SQL query to retrieve states starting with 'N' """
+    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
 
-# Execute the SQL query to retrieve states starting with 'N'
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    """Fetch all the rows from the result set"""
+    results = cursor.fetchall()
 
-# Fetch all the results
-results = cursor.fetchall()
+    """Print the results"""
+    for row in results:
+        print(row)
 
-# Display the results
-for row in results:
-    print(row)
+    """Close the cursor and the database connection"""
+    cursor.close()
+    db.close()
 
-# Close the cursor and database connection
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script_name.py <username> <password> <database>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_states_with_N(username, password, database)
