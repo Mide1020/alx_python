901a444a6324eafb2dc8cf37b88edad5c44dc7e6
mide1020 <akinrefonayomide1@gmail.com>
2023-10-11 03:25:58 +0100

    models

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,27 +1,20 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/python3
+"""
+This is a module that creates python file
+"""
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+
+"""Create a declarative base"""
 Base = declarative_base()
+"""Class for state base"""
+
 
 class State(Base):
-    """
-    Represents a state entity in a database.
-
-    This class defines the structure of the 'states' table, including its columns and constraints.
-
-    Attributes:
-        id (int): An auto-generated, unique integer representing the primary key of the state.
-            It cannot be null and is automatically incremented.
-        name (str): A string representing the name of the state, with a maximum length of 128 characters.
-            It cannot be null.
-
-    Note:
-        This class is intended to be used with SQLAlchemy to interact with a MySQL database.
-        To create the corresponding table in the database, use Base.metadata.create_all(engine),
-        where 'engine' is a SQLAlchemy engine instance connected to the database.
-    """
-
+    """Define the State class"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
