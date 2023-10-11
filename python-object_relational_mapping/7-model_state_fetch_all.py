9a074210c24253299ccb9ab49b4d24070da5c367
mide1020 <akinrefonayomide1@gmail.com>
2023-10-11 03:26:42 +0100

    7-models

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,38 +1,34 @@
+#!/usr/bin/python3
+"""
+This module  lists all State objects
+"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 4:
-    print("Usage: python list_states.py <mysql_username> <mysql_password> <database_name>")
-    sys.exit(1)
 
-# Get command-line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+def list_states(username, password, db_name):
+    """Create an engine to connect to the MySQL server"""
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{db_name}'
+    )
 
-# Define the database connection URL
-db_url = f"mysql://{mysql_username}:{mysql_password}@localhost:3306/{database_name}"
-
-try:
-    # Create a SQLAlchemy engine
-    engine = create_engine(db_url)
-
-    # Create a session to interact with the database
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and list all State objects, sorted by states.id
+    # Retrieve and display State objects sorted by ID
     states = session.query(State).order_by(State.id).all()
-
-    # Display the results in the specified format
     for state in states:
         print(f"{state.id}: {state.name}")
 
-except Exception as e:
-    print(f"Error: {e}")
-finally:
-    # Close the session
     session.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python list_states.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, db_name)
