740fc8edb3ee92c110a60c1afe11f9fbe5bd2224
mide1020 <akinrefonayomide1@gmail.com>
2023-10-11 03:24:39 +0100

    5-filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,44 +1,52 @@
+#!/usr/bin/python3
+"""This module will be Sql injection free
+and it will list id,cities and state in ASC
+order
+"""
+
 import MySQLdb
 import sys
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 5:
-    print("Usage: python list_cities_by_state.py <mysql_username> <mysql_password> <database_name> <state_name>")
-    sys.exit(1)
 
-# Get command-line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_name = sys.argv[4]
-
-try:
-    # Connect to the MySQL server
+def list_cities_by_state(username, password, database, state_name):
+    """Connect to the MySQL server"""
     db = MySQLdb.connect(
-        host="localhost",
+        host='localhost',
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+        user=username,
+        passwd=password,
+        db=database
     )
 
-    # Create a cursor object to interact with the database
+    """Create a cursor object to interact with the database"""
     cursor = db.cursor()
 
-    # Use a prepared statement to prevent SQL injection
-    query = "SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
+    """Execute the SQL query to retrieve cities of the specified state"""
+    query = ("SELECT cities.id, cities.name, states.name "
+             "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "WHERE states.name = %s "
+             "ORDER BY cities.id ASC")
     cursor.execute(query, (state_name,))
 
-    # Fetch all the results
+    """Fetch all the rows from the result set"""
     results = cursor.fetchall()
 
-    # Display the results as a comma-separated string
-    city_names = ', '.join(row[0] for row in results)
-    print(city_names)
+    """Extract and print only the city names"""
+    city_names = [row[1] for row in results]
+    print(", ".join(city_names))
 
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
-finally:
-    # Close the cursor and database connection
+    """Close the cursor and the database connection"""
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script_name.py <username> <password> "
+              "<database> <state_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+        list_cities_by_state(username, password, database, state_name)
